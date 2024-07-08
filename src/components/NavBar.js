ee05eae096c5f24caa395bbf0660809182fecc6e
Dámaris Guzmán <damarisguzman@MacBook-Air-de-Damaris.local>
2024-07-08 09:23:32 +0200

    link navbar actualizados

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { RiAccountCircleLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 const NavbarComponent = () => {
   return (
     <Navbar style={{ backgroundColor: '#136ebf' }} variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="../pages/Inicio.js">Tienda de Cursos</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Tienda de Cursos</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/../pages/Inicio.js">Inicio</Nav.Link>
-            <Nav.Link href="../pages/CursosVenta.js">Cursos</Nav.Link>
-            <Nav.Link href="../pages/Comunidad.js">Comunidad</Nav.Link>
-            <Nav.Link href="../pages/CursosComprados.js">Mis cursos</Nav.Link>
-            <Nav.Link href="../pages/Login.js"><RiAccountCircleLine size={24}  /></Nav.Link>
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/cursos-venta">Cursos</Nav.Link>
+            <Nav.Link as={Link} to="/comunidad">Comunidad</Nav.Link>
+            <Nav.Link as={Link} to="/cursos-comprados">Mis cursos</Nav.Link>
+            <Nav.Link as={Link} to="/login"><RiAccountCircleLine size={24}  /></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
